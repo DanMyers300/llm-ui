5fb1295b7a957c46c452d90e833c90d47243c3e4
Dan <contact+github@danmyers.net>
2023-12-03 22:04:27 -0600

    updated baseurl

--- src/services/appConfig.ts
+++ src/services/appConfig.ts
@@ -5,7 +5,7 @@ import { computed } from 'vue'
 export const gravatarEmail = useLocalStorage('gravatarEmail', 'contact@danmyers.net')
 export const avatarUrl = computed(() => gravatarUrl(gravatarEmail.value, { size: 200 }))
 export const debugMode = useLocalStorage('debug', false)
-export const baseUrl = useLocalStorage('baseUrl', 'https://d34742c6cbf814328ac4dbdf4122335a4.clg07azjl.paperspacegradient.com')
+export const baseUrl = useLocalStorage('baseUrl', 'https://d34742c6cbf814328ac4dbdf4122335a4.clg07azjl.paperspacegradient.com/api')
 export const isDarkMode = useLocalStorage('darkMode', true)
 export const isSettingsOpen = useLocalStorage('settingsPanelOpen', true)
 export const toggleSettingsPanel = () => (isSettingsOpen.value = !isSettingsOpen.value)
